0435ae4de7aa4e72ace29d1f758c2f2281644ad6
Md. Amir Hossain Jowel <mdamirhossainjowel@gmail.com>
2022-05-08 00:44:22 +0600

    single data

--- src/Component/ManageProducts/ManageProducts.js
+++ src/Component/ManageProducts/ManageProducts.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ManageProducts = () => {
   const { item_id } = useParams();
+  const { product, setProduct } = useState([]);
+
+  fetch(`http://localhost:5000/product/${item_id}`)
+    .then((res) => res.json())
+    .then((data) => setProduct(data));
+
   return (
     <div className="container mx-auto text-center my-10">
-      <h1>Hello {item_id}</h1>
-      <button>Deliverd </button>
+      <div className="w-1/2 mx-auto">
+        <h1>{product?.Name}</h1>
+        <h1>{product?.Seller}</h1>
+      </div>
+
       <div className="w-1/2 mx-auto">
         <form>
           <input type="text" placeholder="Restock Item" />
